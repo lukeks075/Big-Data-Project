f898425815cbdfcbccea8b5b6d7f26b791806a80
GratienM <gratienmbono@gmail.com>
2026-06-04 21:32:58 +0200

    Airflow DAG

---
+++ dags/football_weather_dag.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+# Configuration du DAG
+default_args = {
+    'owner': 'data_team',
+    'depends_on_past': False,
+    'start_date': datetime(2026, 6, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'football_weather_pipeline',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+
+    # 1. Ingestion des données (Football & Weather)
+    task_ingest = BashOperator(
+        task_id='ingest_data',
+        bash_command='python /Users/gratien/Downloads/Big-Data-Project/ingest_football.py && python /Users/gratien/Downloads/Big-Data-Project/ingest_weather.py'
+    )
+
+    # 2. Transformation DBT (Le cœur du calcul)
+    task_transform = BashOperator(
+        task_id='dbt_run',
+        bash_command='dbt run --project-dir /Users/gratien/Downloads/Big-Data-Project/'
+    )
+
+    # 3. Indexation Elasticsearch
+    task_index = BashOperator(
+        task_id='index_to_elastic',
+        bash_command='python /Users/gratien/Downloads/Big-Data-Project/index_to_elasticsearch.py'
+    )
+
+    # Définition de la séquence
+    task_ingest >> task_transform >> task_index
